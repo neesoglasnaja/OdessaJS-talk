6b148c98521ee81ea7be6f86427417aca9ebc20c
Nikita Dubko <mefody93@gmail.com>
2018-06-30 19:19:15 +0300

    Add more pictures

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,19 @@
-var gulp = require('gulp'),
-    jade = require('gulp-jade'),
-    stylus = require('gulp-stylus'),
-    watch = require('gulp-watch');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
 
-var config = {
-    stylus: "./src/styles/*.styl",
-    jade:'./src/*.jade',
-    html: "./*.html",
-    dist: "./"
-};
+const gutil = require('gulp-util');
+const usage = require('gulp-help-doc');
 
-gulp.task('jade', function() {
-  return gulp.src(config.jade)
-    .pipe(jade({
-      pretty: true
-    }))
-    .pipe(gulp.dest(config.dist))
-    .pipe(browserSync.stream());
-});
+require('require-dir')('./gulp-tasks');
 
-gulp.task('stylus', function () {
-  return gulp.src(config.stylus)
-    .pipe(stylus())
-    .pipe(gulp.dest(config.dist))
-    .pipe(browserSync.stream());
-});
+/**
+ * Prints this help text
+ *
+ * @task {help}
+ * @group {Misc}
+ */
+gulp.task('help', () => usage(gulp, {
+    logger: gutil,
+    emptyLineBetweenTasks: false,
+}));
 
-gulp.task('default', function() {
-    browserSync.init({
-        server: {
-            baseDir: config.dist
-        }
-    });
-    gulp.watch(config.jade, function(event) {
-        gulp.run('jade');
-    });
-    gulp.watch(config.stylus, function(event) {
-        gulp.run('stylus');
-    });
-    gulp.watch(config.html).on('change', browserSync.reload);
-});
+gulp.task('default', gulp.series('serve'));
